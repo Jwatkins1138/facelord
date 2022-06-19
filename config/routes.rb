40753166dcde75dffd3cdf5d252f8fa8ecae4109
Jason Watkins <goushoryuken@gmail.com>
2022-06-19 13:37:34 -0700

    more styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resource :users
-  resource :posts
+  resources :users
+  resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
