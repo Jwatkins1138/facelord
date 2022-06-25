bf12fdfc1950273ed8d9e99028b3eb7ddcfcdc10
Jason Watkins <goushoryuken@gmail.com>
2022-06-25 16:16:19 -0700

    fixed friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   has_many :comments
   has_many :post_likes
   has_many :liked_posts, :through => :post_likes, :source => :post
+
   has_many :friendships, :foreign_key => :accepting_friend_id
   has_many :friendships, :foreign_key => :inviting_friend_id 
 
@@ -22,22 +23,33 @@ class User < ApplicationRecord
                 association_foreign_key: :inviting_friend_id              
   
                  
+  def pending_invite_friends
+    (invited_friends.joins(:friendships).merge(Friendship.pending))
+  end
+
+  def confirmed_invite_friends
+    (invited_friends.joins(:friendships).merge(Friendship.accepted))
+  end
+
+  def pending_accept_friends
+    (accepted_friends.joins(:friendships).merge(Friendship.pending))
+  end
+
+  def confirmed_accept_friends
+    (accepted_friends.joins(:friendships).merge(Friendship.accepted))
+  end
 
   def friends
-    (invited_friends.joins(:friendships).merge(Friendship.accepted) + accepted_friends.joins(:friendships).merge(Friendship.accepted)).flatten.uniq
+    (confirmed_invite_friends + confirmed_accept_friends).flatten.uniq
   end
 
   def feed
     (self.friends << self)
   end
 
-  def pending_invite_friends
-    (invited_friends.joins(:friendships).merge(Friendship.pending))
-  end
+  
 
-  def pending_accept_friends
-    (accepted_friends.joins(:friendships).merge(Friendship.pending))
-  end
+  
 
 
   # Include default devise modules. Others available are:
