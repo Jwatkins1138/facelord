bf6af775342f42b7fd21d0fbd8cabc9383a225d0
Jason Watkins <goushoryuken@gmail.com>
2022-06-26 10:52:53 -0700

    fixed sign up view

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
   has_many :comments
   has_many :post_likes
   has_many :liked_posts, :through => :post_likes, :source => :post
-
   has_many :friendships, :foreign_key => :accepting_friend_id
   has_many :friendships, :foreign_key => :inviting_friend_id 
 
@@ -46,11 +45,6 @@ class User < ApplicationRecord
   def feed
     (self.friends << self)
   end
-
-  
-
-  
-
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
